eb7d40ddb09c8e1cd1bc2684240d621071cbec20
O-COOL <91911551+DmcTuGfa@users.noreply.github.com>
2026-04-07 16:57:30 -0600

    Update latam-map.tsx

--- components/latam-map.tsx
+++ components/latam-map.tsx
@@ -97,13 +97,13 @@ export function LatamMap({ onCountryClick }: LatamMapProps) {
     projection="geoMercator"
     projectionConfig={{
       scale: 400,
-      center: [-75, -8],
+      center: [-75, 0],
     }}
     className="w-full h-auto"
     style={{ maxHeight: "700px" }}
   >
     <ZoomableGroup
-      center={[-75, -8]}
+      center={[-75, 0]}
       zoom={1}
       minZoom={1}
       maxZoom={1}
